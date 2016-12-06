46e5b904a146350465c8a33c265c954c2e4a2249
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2016-12-06 22:54:20 +0000

    Added gitignore to keep my keys separate from the sender function, organised code into a function so it will be easier to call it in the future funtions

--- mailSender.py
+++ mailSender.py
@@ -1,31 +1,31 @@
 import smtplib
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
+from keys import * # I keep my personal information in a separate file
 
-user_email = ""
-passw = ""
-sendTo = ""
-sub = "New alarm"
-image = ""
+def sendNewMail():
+	#define your personal information or in a separate file as I did
+	#user_email =""
+	#passw = ""
+	#sendTo = ""
+	sub = "New alarm"
+	image = "icon.png"
+	message = MIMEMultipart()
+	message["Subject"] = sub
+	message["From"] = user_email
+	message["To"] = sendTo
+	message.premble = "alert"
 
-message = MIMEMultipart()
+	fp = open(image,"rb")
+	image = MIMEImage(fp.read())
+	fp.close()
+	
+	message.attach(image)
 
-message['Subject'] = sub
-message['From'] = user_email
-message['To'] = sendTo
-message.premble = "alert"
-
-fp = open(image,"rb")
-image = MIMEImage(fp.read())
-fp.close()
-
-message.attach(image)
-
-sendIt = smtplib.SMTP("smtp.gmail.com",587)
-sendIt.ehlo()
-sendIt.starttls()
-sendIt.ehlo()
-sendIt.login(user_email,passw)
-sendIt.send_message(message)
-sendIt.quit()
-
+	sendIt = smtplib.SMTP("smtp.gmail.com",587)
+	sendIt.ehlo()
+	sendIt.starttls()
+	sendIt.ehlo()
+	sendIt.login(user_email,passw)
+	sendIt.send_message(message)
+	sendIt.quit()
