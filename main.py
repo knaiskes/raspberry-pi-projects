b6cc548140a455f10afa6bee1765cc9968418138
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2016-12-14 00:09:31 +0200

    fswebcam requires root permissions, added sudo in the command

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from os import system
 
 while True:
 	if(runSensor()):
-		system("fswebcam -r 1280x720 --no-banner image.jpg")
+		system("sudo fswebcam -r 1280x720 --no-banner image.jpg")
 		sendNewMail()
 
