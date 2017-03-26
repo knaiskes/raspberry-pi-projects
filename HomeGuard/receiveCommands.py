cedcb5513cd72c4bdb0347351e0e1bc0a4eefbab
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2017-03-26 22:21:06 +0300

    merged project

---
+++ HomeGuard/receiveCommands.py
@@ -0,0 +1,19 @@
+import imaplib
+import email
+from keys import *
+
+def receiveEmail():
+	account = imaplib.IMAP4_SSL("imap.gmail.com")
+	account.login(user_email,passw)
+	account.list() #get the list of default directories
+	account.select("inbox")
+	result,data = account.search(None,"ALL")
+	mail = data[0]
+	mail_list = mail.split()
+	getMail = mail_list[-1]
+	result,data = account.fetch(getMail,"(RFC822)")
+	decode_mail = data[0][1]
+	message = email.message_from_string(decode_mail.decode("utf-8"))
+	print(email.utils.parseaddr(message["From"]))
+	print(message["Subject"])
+	
