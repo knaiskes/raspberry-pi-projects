2720d1f82c769ca49d56212dd65ef0e415eb99b8
Kiriakos <kiriakosnaiskes@gmail.com>
2017-07-20 20:56:57 +0300

    added raspberry pi code to light up the led

--- lichessStream/lichessStream.py
+++ lichessStream/lichessStream.py
@@ -1,12 +1,21 @@
+#!/usr/bin/env/python3
+
 import json
 import requests
 from time import sleep
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+ledPin = 17
 
 favoriteList = [] # append the list with your favorite lichess.org streamers
 
 keyword = "lichess.org"
 client_id = "" # enter your client id
 query = "https://api.twitch.tv/kraken/search/streams?query={0}/&client_id={1}".format(keyword,client_id)
+
+GPIO.setup(ledPin,GPIO.OUT)
 
 while(True):
 	try:
@@ -18,12 +27,13 @@ while(True):
 			name = stream["channel"]["display_name"]
 			tmpList.append(name)
 		if(len(list(set(favoriteList).intersection(tmpList))) > 0):
-			print("light on")
+			GPIO.output(ledPin,GPIO.HIGH)
 		else:
-			print("light off")
+			GPIO.output(ledPin,GPIO.LOW)
 		sleep(45)
 	except KeyError:
 		print("There is something wrong with your client id")
 		exit(0)
 	except KeyboardInterrupt:
+		GPIO.output(ledPin,GPIO.LOW)
 		exit(0)
