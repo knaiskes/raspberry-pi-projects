6306ce153908b896730bf3c019a5e312467e2949
Kiriakos <kiriakosnaiskes@gmail.com>
2017-07-26 16:15:29 +0300

    finished projected, added error handler

--- websiteWatcher/websiteWatcher.py
+++ websiteWatcher/websiteWatcher.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env/python3
 
+# simple script to check if my website is up and running fine or if there is
+# any problem with it
+
 import urllib.request
 import RPi.GPIO as GPIO
 
@@ -7,23 +10,30 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 redLed = 17
-blueLed = 18
+greenLed = 18
 
 GPIO.setup(redLed,GPIO.OUT)
-GPIO.setup(blueLed,GPIO.OUT)
+GPIO.setup(greenLed,GPIO.OUT)
 
 mySite = ""
 
 while(True):
-	if(urllib.request.urlopen(mySite).getcode() == 200):
-		# site is up and running
-		GPIO.output(redLed,GPIO.LOW) 
-		GPIO.output(blueLed,GPIO.HIGH)
+	try:
 
-	else:
-		# something wrong is going on
-		GPIO.output(blueLed,GPIO.LOW)
-		GPIO.output(redLed,GPIO.HIGH)
+		if(urllib.request.urlopen(mySite).getcode() == 200):
+			# site is up and running
+			GPIO.output(redLed,GPIO.LOW) 
+			GPIO.output(greenLed,GPIO.HIGH)
+
+		else:
+			# something wrong is going on
+			GPIO.output(greenLed,GPIO.LOW)
+			GPIO.output(redLed,GPIO.HIGH)
+
+	except KeyboardInterrupt:
+		GPIO.output(greenLed,GPIO.LOW)
+		GPIO.output(redLed,GPIO.LOW)
+		exit(0)
 
 
 
