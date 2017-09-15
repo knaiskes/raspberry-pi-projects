a2eb58ffd840981fb82b48c814ddca7a6dfab4dc
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2017-09-15 12:12:58 +0000

    fixed sqlite3 programmingError

--- database/database.py
+++ database/database.py
@@ -1,24 +1,30 @@
 import sqlite3
 
-conn = sqlite3.connect("users.db")
-
-db = conn.cursor()
-
+database = "users.db"
 
 def createDB():
+	conn = sqlite3.connect(database)
+	db = conn.cursor()
 	db.execute("CREATE TABLE IF NOT EXISTS users (name text, password text)")
+	conn.commit()
+	db.close()
 
 def addUser(name, password):
+	conn = sqlite3.connect(database)
+	db = conn.cursor()
 	db.execute("INSERT INTO users VALUES(?,?)",(name,password,))
+	conn.commit()
+	db.close()
 
 
 def checkUser(name, password):
+	conn = sqlite3.connect(database)
+	db = conn.cursor()
 	db.execute("SELECT * FROM users WHERE name = ? AND password = ?",(name,password,))
 	exist = db.fetchone()
 	if exist is None:
 		return False
 	else:
 		return True
-	
-conn.commit()
-conn.close()
+
+	db.close()
